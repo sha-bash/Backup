ab3dc90632f974a1bbfb3852f942eab7f65a8b3d
sha-bash <shabashev.va@yandex.ru>
2024-02-23 20:02:09 +0400

    update main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Vk.vk_apis import VK_API_Client
 from Yandex_Disk.yandex_disk_api import Yandex_Disk_API_Client
+from tqdm import tqdm
+import time
 
 def backup(user_id, token_yandex):
     vk_client = VK_API_Client(user_id)
@@ -7,6 +9,10 @@ def backup(user_id, token_yandex):
 
     vk_client.get_photo()
     yandex_disk_client.save_photo()
+    for _ in tqdm(range(8), desc='Загрузка фото'):
+        time.sleep(1)
+
     print('Загрузка фото завершена')
+
     
 backup(user_id = input('Введите Ваш UserId: '), token_yandex = input('Введите Ваш токен Яндекс.Диска: '))
